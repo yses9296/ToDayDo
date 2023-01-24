0ccd8d00d4c64dc9a320d656de253ace8b004d56
Eunseo Choi <yses9296@gmail.com>
2023-01-25 01:36:00 +0900

    Update func continue..

--- components/ToDoView.js
+++ components/ToDoView.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Text,  TextInput, ScrollView, Dimensions  } from 'react-native';
+import { StyleSheet, View, Text, TextInput, ScrollView, TouchableOpacity, Dimensions, Alert  } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Fontisto } from '@expo/vector-icons'; 
 import { theme } from '../colors.js';
@@ -12,10 +12,11 @@ const ToDoView = () => {
     const onChangeText = (e) => setTask(e);
 
     const [todos, setTodos] = useState({});
+    const [toggle, setToggle] = useState(false);
 
     useEffect(() => {
         loadToDos();
-    },[])
+    },[toggle])
 
     const saveTodos = async (_todo) => {
         try {
@@ -38,26 +39,68 @@ const ToDoView = () => {
         if(task === "") {
             return
         }
-        const newTodos = {...todos, [Date.now()]: {text: task} }
+        const newTodos = {...todos, [Date.now()]: {text: task, completed: false} }
         setTodos(newTodos);    
         await saveTodos(newTodos);
         //reset
         setTask('');
     }
+    const removeTaskHandler = async (id) => {
+        Alert.alert(
+            "Delete To Do", 
+            "Are you sure?", 
+            [
+                {text: "Cancel"}, 
+                {text: "Delete", style: "destructive", onPress: () => {
+                    const newTodos = {...todos}
+                    delete newTodos[id]
+            
+                    setTodos(newTodos);    
+                    saveTodos(newTodos);
+                }}
+            ]
+        )
+    }
+    const toggleCheck = (id) => {
+        const newTodos = {...todos}
+        console.log(newTodos)
+  
+        if(newTodos[id].completed == true){
+            return newTodos[id].completed = false
+        }
+        if(newTodos[id].completed == false) {
+            return newTodos[id].completed = true
+        }
+
+        setToggle( prev => !prev )
+        setTodos(newTodos);    
+        saveTodos(newTodos);
+        
+    }
+
     return (
         <View>
             <TextInput 
-            style={styles.taskInput}
-            onChangeText={text => onChangeText(text)}
-            onSubmitEditing={addTaskHandler}
-            value={task}
-            placeholder= 'add a new task'
-            returnKeyType='done'
+                style={styles.taskInput}
+                onChangeText={text => onChangeText(text)}
+                onSubmitEditing={addTaskHandler}
+                value={task}
+                placeholder= 'add a new task'
+                returnKeyType='done'
             />
             <ScrollView>
             {Object.keys(todos).reverse().map( key => (
                 <View key={key} style={styles.task}>
+ 
+                    { todos[key].completed == false
+                    ? <Fontisto name="checkbox-passive" size={24} color="black" style={styles.checkBox} onPress={() => toggleCheck(key) }/> 
+                    : <Fontisto name="checkbox-active" size={24} color="black" style={styles.checkBox} onPress={() => toggleCheck(key) }/>}
                     <Text style={styles.taskText}>{todos[key].text}</Text>
+
+                    <TouchableOpacity onPress={ () => removeTaskHandler(key) }>
+                        <Fontisto name="trash" size={20} color={theme.grey} />
+                    </TouchableOpacity>
+                    
                 </View>
             ))}
             </ScrollView>
@@ -67,26 +110,34 @@ const ToDoView = () => {
 
 const styles = StyleSheet.create({
     taskInput: {
-      backgroundColor: theme.white,
-      borderRadius: 10,
-      paddingVertical: 12,
-      paddingHorizontal: 20,
-      marginVertical: 20,
-      fontSize: 18,
+        backgroundColor: theme.white,
+        borderRadius: 10,
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        marginVertical: 20,
+        fontSize: 18,
     },
   
     task: {
-      backgroundColor: theme.toDoBg,
-      marginBottom: 10,
-      paddingVertical: 20,
-      paddingHorizontal: 40,
-      borderRadius: 5
+        backgroundColor: theme.toDoBg,
+        marginBottom: 10,
+        paddingVertical: 16,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+        flexDirection: 'row',
+        alignItems:'center',
     },
     taskText: {
-      color: theme.white,
-      fontSize: 18,
-      fontWeight: '500'
+        flex: 5,
+        color: theme.white,
+        fontSize: 18,
+        fontWeight: '500'
     },
+
+    checkBox: {
+        flex: 1,
+        marginRight: 20
+    }
 
 });
 
