300476d5de2a6682e21bf4d9a7448abdabb52808
Eunseo Choi <yses9296@gmail.com>
2023-01-24 23:17:45 +0900

    Calendar func and selected date

--- App.js
+++ App.js
@@ -10,10 +10,11 @@ import { theme } from './colors.js';
 export default function App() {
   const [activeTodo, setActiveTodo] = useState(true);
   const clickTodo = () => setActiveTodo(true);
-  const clickDiary = () => setActiveTodo(false);
+  const clickCalendar = () => setActiveTodo(false);
 
   const [task, setTask] = useState('');
-  const [dairy, setDairy] = useState('');
+  const onChangeText = (e) => setTask(e);
+  const addTaskHandler = () => {}
 
   return (
     <View style={styles.container}>
@@ -25,26 +26,29 @@ export default function App() {
             <Fontisto name="nav-icon-list-a" size={24} color="white" />
           </Text>
         </Pressable>
-        <Pressable onPress={clickDiary}>
+        <Pressable onPress={clickCalendar}>
           <Text style={{...styles.headerText, color: !activeTodo ? theme.white : theme.grey}}>
             <Fontisto name="calendar" size={24} color="white" />
           </Text>
         </Pressable>
       </View>
-      <View>
-        {activeTodo ? (
-          <SafeAreaView>
-            <TextInput 
-              style={styles.taskInput}
-              onChangeText={text => onChangeText(text)}
-              value={task}
-              placeholder= 'New Task'
-              />
-          </SafeAreaView>
-          ) : (
-            <CalendarView/>
-        )}
-      </View>
+  
+      {activeTodo ? (
+        <View>
+          <TextInput 
+            style={styles.taskInput}
+            onChangeText={text => onChangeText(text)}
+            value={task}
+            multiline
+            placeholder= 'New Task'
+          />
+          <Button 
+            onPress={addTaskHandler}
+            title="Add"
+          />
+        </View>
+      ) : ( <CalendarView/> )}
+  
     </View>
   );
 }
--- components/CalendarView.js
+++ components/CalendarView.js
@@ -1,11 +1,71 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import { format } from "date-fns";
 import { Calendar } from "react-native-calendars";
+import uuid from 'react-native-uuid';
+import { theme } from '../colors';
 
 const CalendarView = () => {
+  const [event, setEvent] = useState('');
+  const generateId = 1;
+  const posts = [
+    {
+      id: 1,
+      title: "First Title.",
+      contents: "First Content.",
+      date: "2023-01-26",
+    }
+  ];
+
+  const markedDates = posts.reduce((acc, c) => {
+    const formattedDate = format(new Date(c.date), 'yyyy-MM-dd');
+    acc[formattedDate] = {marked: true};
+    return acc;
+  }, {});
+
+  const [selectedDate, setSelectedDate] = useState( format(new Date(), "yyyy-MM-dd") );
+  
+  const markedSelectedDates = {
+    ...markedDates,
+    [selectedDate]: {
+      selected: true,
+      marked: markedDates[selectedDate]?.marked,
+    }
+  }
+
+  const onChangeText = (e) => setEvent(e);
+  const addEventHandler = () => {
+    alert(selectedDate)
+  }
+
   return (
     <View>
-        <Calendar style={styles.calendar} />
+        <Calendar 
+          style={styles.calendar} 
+          markedDates={markedSelectedDates}
+          theme={{
+            selectedDayBackgroundColor: 'red',
+            selectedDayTextColor: '#ffffff',
+            todayTextColor: '#183641',
+            arrowColor: 'blue',
+          }}
+          onDayPress={day => setSelectedDate(day.dateString)}
+        />
+
+        <View>
+          <Text style={styles.date}>{selectedDate}</Text>
+          <TextInput 
+            style={styles.input}
+            onChangeText={text => onChangeText(text)}
+            placeholder='New Event'
+            value={event}
+          />
+          <Button 
+            style={styles.addBtn}
+            onPress={addEventHandler}
+            title="Add"
+          />
+        </View>
     </View>
   )
 }
@@ -14,7 +74,23 @@ const styles = StyleSheet.create({
       padding: 20,
       marginVertical: 20,
       borderRadius: 10
-    }
+    },
+    date: {
+      color: theme.white,
+      textAlign: 'center',
+      fontSize: 24
+    },
+    input: {
+      backgroundColor: theme.white,
+      borderRadius: 20,
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      marginVertical: 20,
+      fontSize: 18
+    },
+    addBtn: {
+
+    },
 });
 
 export default CalendarView
